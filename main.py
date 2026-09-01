ad70d9d153c6d2ab85af309df1f303578958e91f
RegroupForces <161395559+RegroupForces@users.noreply.github.com>
2026-09-01 20:50:38 +1000

    Initial Upload

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+# Code for Approach Height Detection Subsystem
+# Created by: Jianqiu (Jacky) Dong
+# Date Created: 26/08/2026
+# Last Modified: 01/09/2026
+
+
+
 from pymata4 import pymata4
+
+
+def detectUltrasonic():
+    """
+    Helper function that attempts to take a reading from the untrasonic sensors.
+
+
+    Args:
+        None
+
+    Returns:
+        height: float
+        The post processed height value. Calculated from the voltage reading from the Ultrasonic Sensor.
+    """
+    pass
+
+
+def main():
+    """
+    
+    
+    
+    """
+
+    pass
